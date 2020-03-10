9a2216558f2fd0351d0c1ab4721078df8705a84c
CloserWU <18969946575@163.com>
2020-03-10 20:52:06 +0800

    graph

--- Java/src/main/java/com/closer/Graph.java
+++ Java/src/main/java/com/closer/Graph.java
@@ -43,7 +43,28 @@ public class Graph {
         }
     }
 
-    Map<String, GNode> graph = new HashMap<>();
+    private Map<String, GNode> graph = new HashMap<>();
+    private Map<String, Boolean> visit = new HashMap<>();
+
+
+    void initialVisit() {
+        for (Map.Entry<String, Boolean> entry : visit.entrySet()) {
+            entry.setValue(false);
+        }
+    }
+
+    void initialGraph() {
+        for (Map.Entry<String, GNode> entry : graph.entrySet()) {
+            GNode gNode = entry.getValue();
+            Edge edge = gNode.head;
+            Edge tmp = null;
+            while (edge != null) {
+                tmp = edge;
+                edge = edge.next;
+                tmp = null;
+            }
+        }
+    }
 
     void insertEdge(String key1, String key2, int weight) {
         // 如果这个顶点以及存在，则将边加入这个顶点 （GNode 的 Edge；Edge的链表使用头插法）
@@ -58,6 +79,7 @@ public class Graph {
             GNode gNode = new GNode(key1, edge);
             graph.put(key1, gNode);
         }
+        visit.put(key1, false);
     }
 
     void showGraph() {
@@ -73,13 +95,157 @@ public class Graph {
         }
     }
 
+
+    int BFS() {
+        int component = 0;
+        for(Map.Entry<String, GNode> entry : graph.entrySet()) {
+            component++;
+            /*Queue<GNode> queue = new PriorityQueue<>(new Comparator<GNode>() {
+                @Override
+                public int compare(GNode o1, GNode o2) {
+                    return o1.nodeName.compareTo(o2.nodeName);
+                }
+            });*/
+            Queue<GNode> queue = new LinkedList<>();
+            if (visit.get(entry.getKey())) {
+                continue;
+            }
+            queue.add(entry.getValue());
+            while (!queue.isEmpty()) {
+                GNode gNode= queue.poll();
+                Edge edge = gNode.head;
+                System.out.print(" --- " + gNode.nodeName);
+                visit.put(gNode.nodeName, true);
+                while (edge != null) {
+                    if (!visit.get(edge.nodeName)) {
+                        queue.add(graph.get(edge.nodeName));
+                        visit.put(edge.nodeName, true);
+                    }
+                    edge = edge.next;
+                }
+            }
+            System.out.println();
+        }
+        return component;
+    }
+
+    int DFS() {
+        int component = 0;
+        for(Map.Entry<String, GNode> entry : graph.entrySet()) {
+            component++;
+            Stack<GNode> stack = new Stack<>();
+            if (visit.get(entry.getKey())) {
+                continue;
+            }
+            stack.push(entry.getValue());
+            while (!stack.isEmpty()) {
+                GNode gNode= stack.peek();
+                stack.pop();
+                Edge edge = gNode.head;
+                System.out.print(" --- " + gNode.nodeName);
+                visit.put(gNode.nodeName, true);
+                while (edge != null) {
+                    if (!visit.get(edge.nodeName)) {
+                        stack.add(graph.get(edge.nodeName));
+                        visit.put(edge.nodeName, true);
+                    }
+                    edge = edge.next;
+                }
+            }
+            System.out.println();
+        }
+        return component;
+    }
+
+    List<Map<String, Integer>> solution() {
+        List<Map<String, Integer>> lm = new LinkedList<>();
+        lm.clear();
+        for (Map.Entry<String, GNode> entry: graph.entrySet()) {
+            Stack<GNode> stack = new Stack<>();
+            if (visit.get(entry.getKey())) {
+                continue;
+            }
+            stack.push(entry.getValue());
+            Map<String, Integer> map = new HashMap<>();
+            map.clear();
+            while (!stack.isEmpty()) {
+                GNode gNode = stack.peek();
+                stack.pop();
+                Edge edge = gNode.head;
+                visit.put(gNode.nodeName, true);
+                while (edge != null) {
+                    if (!visit.get(edge.nodeName)) {
+                        visit.put(edge.nodeName, true);
+                        stack.push(graph.get(edge.nodeName));
+                    }
+                    if (!map.containsKey(gNode.nodeName)) {
+                        map.put(gNode.nodeName, 0);
+                    }
+                    map.put(gNode.nodeName, map.get(gNode.nodeName) + edge.weight);
+                    if (!map.containsKey(edge.nodeName)) {
+                        map.put(edge.nodeName, 0);
+                    }
+                    map.put(edge.nodeName, map.get(edge.nodeName) + edge.weight);
+                    edge = edge.next;
+                }
+            }
+            if (!map.isEmpty() && map.size() >=3) {
+                lm.add(map);
+            }
+        }
+        return lm;
+    }
+
+    class Pair {
+        String str;
+        Integer num;
+        Pair(String str, Integer num) {
+            this.num = num;
+            this.str = str;
+        }
+    }
+
+    void showResult(List<Map<String, Integer>> lm, int threshold) {
+        List<Pair> lp = new LinkedList<>();
+        for (Map<String, Integer> map : lm) {
+            int total = 0;
+            int maxWeight = Integer.MIN_VALUE;
+            String maxString = "";
+            for(Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (entry.getValue() > maxWeight) {
+                    maxWeight = entry.getValue();
+                    maxString = entry.getKey();
+                }
+                total += entry.getValue();
+            }
+            if (total > threshold * 4) {
+                lp.add(new Pair(maxString, map.size()));
+            }
+        }
+        if (!lp.isEmpty()) {
+            lp.sort(new Comparator<Pair>() {
+                @Override
+                public int compare(Pair o1, Pair o2) {
+                    return o1.str.compareTo(o2.str);
+                }
+            });
+            System.out.println(lp.size());
+            for (Pair p: lp) {
+                System.out.println(p.str + " " + p.num);
+            }
+        } else {
+            System.out.println(0);
+        }
+    }
+
+
     public static void main(String[] args) {
         Graph graph = new Graph();
-
-        int n;
+        int n, k;
         Scanner input = new Scanner(System.in);
         while (input.hasNext()) {
             n = input.nextInt();
+            k = input.nextInt();
             graph.graph.clear();
             String str1;
             String str2;
@@ -91,9 +257,15 @@ public class Graph {
                 weight = input.nextInt();
                 // 构造图
                 graph.insertEdge(str1, str2, weight);
+                graph.insertEdge(str2, str1, weight);
             }
             // 展示图
-            graph.showGraph();
+//            graph.showGraph();
+//            graph.DFS();
+            graph.initialVisit();
+//            graph.BFS();
+            List<Map<String, Integer>> list = graph.solution();
+            graph.showResult(list, k);
         }
     }
 }
